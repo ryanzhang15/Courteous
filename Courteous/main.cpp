59395a5d97b3a2beb578814861183655b349fa24
ryanzhang15 <61963316+ryanzhang15@users.noreply.github.com>
2022-07-19 18:03:09 +0800

    Second commit. Made a few tweaks in the comments. Made "shortcuts.h" file.

--- Courteous/main.cpp
+++ Courteous/main.cpp
@@ -3,12 +3,17 @@
 //  Courteous
 //
 //  Created by Ryan Zhang on 19/7/2022.
+//  Editors: Ryan, Kenneth
 //
 
-#include <iostream>
+//  Should accept a filepath from * argv[], as well as some arguments, and attempt to compile a courteouspleaseplease file.
+//  Other than reading and interpreting arguments, this file should NOT include anything else.
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "shortcuts.h"
+
+int main(int argc, const char * argv[]) { //for now, ignore argv[], and instead read filepath from stdin (for debugging). change later.
+    
+    puts("Hello world! Welcome to Courteous++.");
+    
     return 0;
 }
---
+++ Courteous/shortcuts.h
@@ -0,0 +1,81 @@
+//
+//  shortcuts.h
+//  Courteous
+//
+//  Created by Ryan Zhang on 19/7/2022.
+//
+
+//  A shorthand header file, which includes the C++ standard library (or the majority of it),
+//  as well as some other useful shortcuts (which look horrible in code, but work, so it works).
+//  Kenneth, feel free to add anything to shortcuts.h.
+//  Every .cpp should include shortcuts.h.
+
+#ifndef shortcuts_h
+#define shortcuts_h
+
+//includes. C++ standard library.
+#include <cstdlib>
+
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <fstream>
+
+#include <cmath>
+#include <cstring>
+#include <ctime>
+
+#include <deque>
+#include <string>
+#include <stack>
+#include <vector>
+#include <map>
+#include <queue>
+#include <list>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <functional>
+
+//namespaces.
+using namespace std;
+
+//typedefs.
+typedef long long _;
+typedef long long ll;
+typedef int _0;
+typedef double _D;
+typedef unsigned long long u_;
+typedef string str;
+typedef vector<_> v_;
+typedef pair<_, _> _p;
+typedef const long long constant;
+
+//defines.
+#define ef else if
+#define leave exit(0);
+
+#define v(x) vector<x >
+#define v2(x) vector<vector<x > >
+#define v3(x) vector<vector<vector<x > > >
+
+#define q(x) queue<x >
+#define dq(x) deque<x >
+#define s(x) set<x >
+#define st(x) stack<x >
+#define ms(x) multiset<x >
+#define m(x, y) map<x , y >
+#define b(x) bitset<x >
+#define l(x) list<x >
+
+#define i(x) x::iterator
+
+#define pr(x, y) pair< x, y >
+#define mp(x, y) make_pair(x, y)
+
+#endif /* shortcuts_h */
