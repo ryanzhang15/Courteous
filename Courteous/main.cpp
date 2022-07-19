4d592ed26c32bbc92a4cca5c50edb0a759cc26b5
ryanzhang15 <61963316+ryanzhang15@users.noreply.github.com>
2022-07-19 16:26:45 +0800

    Initial Commit

---
+++ Courteous/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Courteous
+//
+//  Created by Ryan Zhang on 19/7/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
